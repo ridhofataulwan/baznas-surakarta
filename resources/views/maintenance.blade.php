80b9fa993f922e08d7c9ce7333009fc7ebfc0672
Ridho Fata Ulwan <84189579+ridhofataulwan@users.noreply.github.com>
2022-12-21 11:06:46 +0700

    Add Maintenance Page

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="csrf-token" content="MnhCgQ50o8IcV8jLxOQoUJl2sPwWzOC53r3psO5m">
+    <title>Baznas Surakarta</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/png" href="http://localhost:8000/assets/img/portfolio/logo/logo3.png" />
+    <!-- Bootstrap Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+    <!-- SimpleLightbox plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="http://localhost:8000/css/styles.css" rel="stylesheet" />
+    <!--Pihak Ke 3-->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--Icon-->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                autoDisplay: false
+            }, 'google_translate_element');
+        }
+    </script>
+    <link href="http://localhost:8000/css/basnaz.css" rel="stylesheet">
+    <!--Chart-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+    </script>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <!--Hero Section-->
+    <div class="container-fluid hero-section" style="width: 100%;height: 350px;">
+        <div class="heading">
+            Website is under maintenance
+            <hr id="hr-hero">
+        </div>
+    </div>
+    <!--Hero Section-->
+    <section class="page-section" id="about">
+        <div class="container">
+            <h1 class="heading-content" style="position:relative;
+            top: 50%;
+            bottom: 0;
+            transform:rotateY(-50%);
+            margin: auto;">
+            </h1>
+        </div>
+    </section>
+    <!--Content-->
+
+    <a href="https://api.whatsapp.com/send?phone=6281393055550&text=Assalamu'alaikum." class="floating-container" target="_blank">
+        <i class="fa fa-whatsapp my-float floating-button"></i>
+    </a>
+    <style>
+        .my-float {
+            margin-top: 16px;
+        }
+    </style>
+    <style>
+        @-webkit-keyframes come-in {
+            0% {
+                -webkit-transform: translatey(100px);
+                transform: translatey(100px);
+                opacity: 0;
+            }
+
+            30% {
+                -webkit-transform: translateX(-50px) scale(0.4);
+                transform: translateX(-50px) scale(0.4);
+            }
+
+            70% {
+                -webkit-transform: translateX(0px) scale(1.2);
+                transform: translateX(0px) scale(1.2);
+            }
+
+            100% {
+                -webkit-transform: translatey(0px) scale(1);
+                transform: translatey(0px) scale(1);
+                opacity: 1;
+            }
+        }
+
+        @keyframes come-in {
+            0% {
+                -webkit-transform: translatey(100px);
+                transform: translatey(100px);
+                opacity: 0;
+            }
+
+            30% {
+                -webkit-transform: translateX(-50px) scale(0.4);
+                transform: translateX(-50px) scale(0.4);
+            }
+
+            70% {
+                -webkit-transform: translateX(0px) scale(1.2);
+                transform: translateX(0px) scale(1.2);
+            }
+
+            100% {
+                -webkit-transform: translatey(0px) scale(1);
+                transform: translatey(0px) scale(1);
+                opacity: 1;
+            }
+        }
+
+        .floating-container {
+            position: fixed;
+            width: 100px;
+            height: 100px;
+            bottom: 0;
+            right: 0;
+            margin: 35px 25px;
+        }
+
+        .floating-container:hover {
+            height: 300px;
+            box-shadow: 0 10px 25px rgba(37, 211, 102, 0);
+        }
+
+        .floating-container:hover .floating-button {
+            box-shadow: 0 10px 25px rgba(37, 211, 102, 0);
+            -webkit-transform: translatey(5px);
+            transform: translatey(5px);
+            -webkit-transition: all 0.3s;
+            transition: all 0.3s;
+        }
+
+        .floating-container .floating-button {
+            position: absolute;
+            width: 65px;
+            height: 65px;
+            background: #25D366;
+            bottom: 0;
+            border-radius: 50%;
+            left: 0;
+            right: 0;
+            margin: auto;
+            color: white;
+            line-height: 65px;
+            text-align: center;
+            font-size: 23px;
+            z-index: 100;
+            box-shadow: 0 10px 25px -5px rgba(37, 211, 102, 0);
+            cursor: pointer;
+            -webkit-transition: all 0.3s;
+            transition: all 0.3s;
+        }
+    </style>
+    <style>
+        a {
+            color: white;
+            text-decoration: none
+        }
+    </style>
+</body>
+<!-- Footer-->
+<footer class="footer-clean main-footer">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 col-sm-6 col-md-5 item mt-2" style="padding-right: 5%;">
+                <ul>
+                    <li class="mt-3">
+                        <p class="heading-left">Alamat Kantor :</p>
+                        <p class="content-left">
+                            Jl. Dr. Moewardi, Kel. Penumping, Kec. Laweyan, Kota Surakarta, Jawa Tengah 57141
+                        </p>
+                    </li>
+                    <li class="mt-3">
+                        <p class="heading-left">Telepon :</p>
+                        <p class="content-left">
+                            <a href="https://wa.me/6281393055550" target="_blank">
+                                <ion-icon name="logo-whatsapp"></ion-icon>&nbsp 081393055550
+                            </a>
+                        </p>
+                    </li>
+                    <li class="mt-3">
+                        <p class="heading-left">Email :</p>
+                        <p class="content-left">
+                            <a href="mailto:baznaskota.surakarta@baznas.go.id" target="_blank">
+                                <ion-icon name="mail-outline"></ion-icon>&nbsp baznaskota.surakarta@baznas.go.id
+                            </a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3 mt-2">
+                <ul>
+                    <li class="mt-2">
+                        <div class="form-check form-check-inline">
+                            <input style="background-color: white;" checked class="form-check-input bg-secondary border-0" type="checkbox" id="inlineCheckbox1" value="option1" />
+                            <label class="form-check-label" for="inlineCheckbox1">Kebijakan Privasi</label>
+                        </div>
+                    </li>
+                    <li class="mt-2">
+                        <div class="form-check form-check-inline">
+                            <input style="background-color: white;" checked class="form-check-input bg-secondary border-0" type="checkbox" id="inlineCheckbox2" value="option1" />
+                            <label class="form-check-label" for="inlineCheckbox2">Syarat & Ketentuan</label>
+                        </div>
+                    </li>
+                    <li class="mt-2">
+                        <div class="form-check form-check-inline">
+                            <input style="background-color: white;" checked class="form-check-input bg-secondary border-0" type="checkbox" id="inlineCheckbox3" value="option1" />
+                            <label class="form-check-label" for="inlineCheckbox3">FAQ</label>
+                        </div>
+                    </li>
+                </ul>
+                <p class="heading-left mt-3">Ikuti Kami :</p>
+                <ul>
+                    <li class="mt-2">
+                        <p class="content-left">
+                            <a href="https://www.facebook.com/baznaskotasurakarta" target="_blank">
+                                <ion-icon name="logo-facebook"></ion-icon>&nbsp baznaskotasurakarta
+                            </a>
+                        </p>
+                    </li>
+                    <li class="mt-2">
+                        <p class="content-left">
+                            <a href="https://www.instagram.com/baznaskota.surakarta/" target="_blank">
+                                <ion-icon name="logo-instagram"></ion-icon>&nbsp baznaskota.surakarta
+                            </a>
+                        </p>
+                    </li>
+                    <li class="mt-2">
+                        <p class="content-left">
+                            <a href="https://twitter.com/BaznaskotaS" target="_blank">
+                                <ion-icon name="logo-twitter"></ion-icon>&nbsp @BaznaskotaS
+                            </a>
+                        </p>
+                    </li>
+                    <li class="mt-2">
+                        <p class="content-left">
+                            <a href="https://www.youtube.com/channel/UChgLCY6zG-hdIWVupT9apVg" target="_blank">
+                                <ion-icon name="logo-youtube"></ion-icon>&nbsp BAZNAS Kota Surakarta
+                            </a>
+                        </p>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="col-md-4 mt-2 pb-2">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d988.7787552347216!2d110.8058122!3d-7.5624366!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a1537a82ba903%3A0x36bd356bb2bc805b!2sBaznas%20Kota%20Surakarta!5e0!3m2!1sid!2sid!4v1666581909904!5m2!1sid!2sid" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="end-copyright container-fluid">
+        <center>
+            <p class="copyright text-white">
+                Copyright © 2022 Baznas Surakarta. All Rights Reserved
+            </p>
+        </center>
+    </div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- SimpleLightbox plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+<!-- Core theme JS-->
+<script src="http://localhost:8000/js/scripts.js"></script>
+<script src="http://localhost:8000/js/zakat.js"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+</html>
