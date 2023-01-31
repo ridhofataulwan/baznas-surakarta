435f7b75598968411288cdb04a25d9932256ca9e
Ridho Fata Ulwan <84189579+ridhofataulwan@users.noreply.github.com>
2023-02-01 03:23:39 +0700

    Fix minor

--- resources/views/admin/penyaluran/add.blade.php
+++ resources/views/admin/penyaluran/add.blade.php
@@ -43,7 +43,7 @@
                                                 </div>
                                             </div>
                                             <div class="form-group row mb-4">
-                                                <label class="col-form-label text-md-right col-12 col-md-3 c   ol-lg-3">Tanggal</label>
+                                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tanggal</label>
                                                 <div class="col-sm-12 col-md-7">
                                                     <input type="date" class="form-control" name="created_at">
                                                 </div>
